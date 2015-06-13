bf19859b2f572da333eba753ffdd4fb679124119
Metaclassing <github@secureobscure.com>
2015-06-13 12:32:00 -0500

    Removed legacy junk that found its way back into the schema file.

--- sql/schema.sql
+++ sql/schema.sql
@@ -30,26 +30,6 @@ CREATE TABLE `bgpmon` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `device_old`
---
-
-DROP TABLE IF EXISTS `device_old`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `device_old` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` text,
-  `prompt` text,
-  `ip` text,
-  `protocol` text,
-  `type` text,
-  `model` text,
-  `function` text,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2025 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Table structure for table `information`
 --
 
@@ -111,33 +91,3 @@ CREATE TABLE `session` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `showcmd_old`
---
-
-DROP TABLE IF EXISTS `showcmd_old`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `showcmd_old` (
-  `id` int(11) NOT NULL,
-  `run` longtext,
-  `version` longtext,
-  `inventory` longtext,
-  `diag` longtext,
-  `module` longtext,
-  `interface` longtext,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2014-01-11 21:40:07
